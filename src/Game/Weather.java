e2e0980730f02f319c773889b918a19f9ddff7ea
Ákos Schneider <koss644@gmail.com>
2020-03-25 21:24:16 +0100

    commentek2

--- src/Game/Weather.java
+++ src/Game/Weather.java
@@ -1,6 +1,7 @@
 package Game;
 
 import Field.Field;
+import Skeleton.Skeleton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ public final class Weather {
     private List<Field> fields = new ArrayList<Field>();
 
     public void Blizzard() {
-
+        Skeleton.Called("Blizzard()");
+        Skeleton.Return();
     }
 }
