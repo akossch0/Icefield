83573554f7efa5731f7463d2a2533cf9aa4bcada
Ákos Schneider <koss644@gmail.com>
2020-03-25 21:16:19 +0100

    commentek

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,14 @@
 package Main;
 
+import Skeleton.Skeleton;
+
 class Main
 {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+
+        Skeleton skeleton = new Skeleton();
+
     }
 }
