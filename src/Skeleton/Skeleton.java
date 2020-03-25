eb789a75651e5c198f07c298f44ad229d2fec949
Balázs Blum <blumbalazs99@gmail.com>
2020-03-25 20:39:01 +0100

    Class updates

--- src/Skeleton/Skeleton.java
+++ src/Skeleton/Skeleton.java
@@ -1,4 +1,15 @@
 package Skeleton;
 
+
+
 public class Skeleton {
+    static int n;
+    public static void Called(String FuncHeader){
+        for (int i = 0;i<n;i++) System.out.print("\t");
+        System.out.println(FuncHeader);
+        n++;
+    }
+    public static void Return(){
+        n--;
+    }
 }
