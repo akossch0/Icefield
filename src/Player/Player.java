058b445a9925c2a49f3dfc4f0262c81206e79033
Bálint <takats>
2020-03-25 19:47:23 +0100

    Player, Eskimo and Researcher:))

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,5 +1,30 @@
 package Player;
+import Item.*;
+import Field.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-    private int actualHealt = 0;
+
+    private Field field = null;
+    private Manager manager = null;
+    private List<Item> items = new ArrayList<Item>();
+    private ClothesEquiped clothes = null;
+
+
+    public int UseAbility (Field f){return 0;}
+    public void IncrHp(){}
+    public void DecrHp(){}
+    public void Step(Field f){}
+    public void Dig(){}
+    public Field getField(){return null;}
+    public void UseItem(Item i, Player target){}
+    public void PickUpItem(){}
+    public void setClothes(ColthesEquiped c){}
+    public void yourTurn(){}
+    public void RemoveItem(Item i){}
+    public boolean isWaterproof(){return true;}
+    public void AcceptItem(Item i){}
+
 }
