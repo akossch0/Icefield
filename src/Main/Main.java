eb167892d5025956547a4837e846ad3a577255f7
Balázs Blum <blumbalazs99@gmail.com>
2020-03-25 21:20:24 +0100

    Class updates

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,15 @@
 package Main;
 
+import Skeleton.*;
+
 class Main
 {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+
+        Skeleton.Initialise();
+
+        Skeleton.Test1();
     }
 }
--- src/Skeleton/Skeleton.java
+++ src/Skeleton/Skeleton.java
@@ -2,14 +2,47 @@ package Skeleton;
 
 
 
+import Item.*;
+import Player.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Skeleton {
-    static int n;
-    public static void Called(String FuncHeader){
+    static int n = 0;
+    static HashMap<Object,String> names;
+    public static void Initialise(){
+        names = new HashMap<>();
+        names.put(Rope.class,"Rope");
+        names.put(Item.class,"Item");
+    }
+    public static void Called(Object object,String FuncHeader){
         for (int i = 0;i<n;i++) System.out.print("\t");
-        System.out.println(FuncHeader);
+        System.out.println(names.get(object)+" "+FuncHeader);
         n++;
     }
     public static void Return(){
         n--;
     }
+
+    public static void Test1(){
+        Player player = new Eskimo();
+        Player target = new Researcher();
+        Item rope = new Rope();
+        player.AcceptItem(rope);
+        try {
+            Runtime.getRuntime().exec("cls");
+        }catch(Exception e){ }
+
+        player.UseItem(rope,target);
+    }
+    public static void Question(){
+        String input;
+
+        BufferedReader obj = new BufferedReader(new InputStreamReader((System.in)));
+        /*    Majd még ki kell egészíteni*/
+
+    }
+
 }
