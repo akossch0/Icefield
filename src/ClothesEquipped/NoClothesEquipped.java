0abf5cce3f29ba61cb668aeedf90fba3acb06485
Balázs Blum <blumbalazs99@gmail.com>
2020-03-29 21:23:16 +0200

    Noclothes ctor javitva

--- src/ClothesEquipped/NoClothesEquipped.java
+++ src/ClothesEquipped/NoClothesEquipped.java
@@ -1,6 +1,18 @@
 package ClothesEquipped;
 import Skeleton.Skeleton;
 public class NoClothesEquipped implements ClothesEquipped {
+
+    /**
+     * Konstruktora NoClothesEquipped-nek
+     * @param
+     */
+    public NoClothesEquipped(){
+
+        Skeleton.addNames(this, "NoClothesEquipped");
+        Skeleton.Called(this, "NoClothesEquipped");
+
+        Skeleton.Return();
+    }
     /**
      * @return hamissal ter vissza; jelentes: a jatekos nem visel buvarruhat
      */
