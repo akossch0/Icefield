ded0eb0114b140c1531af58e2ca44da223912e7a
kp565 <petra.kalocsai12@gmail.com>
2020-04-24 15:28:31 +0200

    testcases.test

--- src/Prototype/TestCases.java
+++ src/Prototype/TestCases.java
@@ -72,94 +72,94 @@ public class TestCases {
                         tests.get(2).ExecuteTest(tests.get(2).getContent(), tests.get(2).getOutputContent());
                         break;
                     case (3):
-
+                        tests.get(3).ExecuteTest(tests.get(3).getContent(), tests.get(3).getOutputContent());
                         break;
                     case (4):
-
+                        tests.get(4).ExecuteTest(tests.get(4).getContent(), tests.get(4).getOutputContent());
                         break;
                     case (5):
-
+                        tests.get(5).ExecuteTest(tests.get(5).getContent(), tests.get(5).getOutputContent());
                         break;
                     case (6):
-
+                        tests.get(6).ExecuteTest(tests.get(6).getContent(), tests.get(6).getOutputContent());
                         break;
                     case (7):
-
+                        tests.get(7).ExecuteTest(tests.get(7).getContent(), tests.get(7).getOutputContent());
                         break;
                     case (8):
-
+                        tests.get(8).ExecuteTest(tests.get(8).getContent(), tests.get(8).getOutputContent());
                         break;
                     case (9):
-
+                        tests.get(9).ExecuteTest(tests.get(9).getContent(), tests.get(9).getOutputContent());
                         break;
                     case (10):
-
+                        tests.get(10).ExecuteTest(tests.get(10).getContent(), tests.get(10).getOutputContent());
                         break;
                     case (11):
-
+                        tests.get(11).ExecuteTest(tests.get(11).getContent(), tests.get(11).getOutputContent());
                         break;
                     case (12):
-
+                        tests.get(12).ExecuteTest(tests.get(12).getContent(), tests.get(12).getOutputContent());
                         break;
                     case (13):
-
+                        tests.get(13).ExecuteTest(tests.get(13).getContent(), tests.get(13).getOutputContent());
                         break;
                     case (14):
-
+                        tests.get(14).ExecuteTest(tests.get(14).getContent(), tests.get(14).getOutputContent());
                         break;
                     case (15):
-
+                        tests.get(15).ExecuteTest(tests.get(15).getContent(), tests.get(15).getOutputContent());
                         break;
                     case (16):
-
+                        tests.get(16).ExecuteTest(tests.get(16).getContent(), tests.get(16).getOutputContent());
                         break;
                     case (17):
-
+                        tests.get(17).ExecuteTest(tests.get(17).getContent(), tests.get(17).getOutputContent());
                         break;
                     case (18):
-
+                        tests.get(18).ExecuteTest(tests.get(18).getContent(), tests.get(18).getOutputContent());
                         break;
                     case (19):
-
+                        tests.get(19).ExecuteTest(tests.get(19).getContent(), tests.get(19).getOutputContent());
                         break;
                     case (20):
-
+                        tests.get(20).ExecuteTest(tests.get(20).getContent(), tests.get(20).getOutputContent());
                         break;
                     case (21):
-
+                        tests.get(21).ExecuteTest(tests.get(21).getContent(), tests.get(21).getOutputContent());
                         break;
                     case (22):
-
+                        tests.get(22).ExecuteTest(tests.get(22).getContent(), tests.get(22).getOutputContent());
                         break;
                     case (23):
-
+                        tests.get(23).ExecuteTest(tests.get(23).getContent(), tests.get(23).getOutputContent());
                         break;
                     case (24):
-
+                        tests.get(24).ExecuteTest(tests.get(24).getContent(), tests.get(24).getOutputContent());
                         break;
                     case (25):
-
+                        tests.get(25).ExecuteTest(tests.get(25).getContent(), tests.get(25).getOutputContent());
                         break;
                     case (26):
-
+                        tests.get(26).ExecuteTest(tests.get(26).getContent(), tests.get(26).getOutputContent());
                         break;
                     case (27):
-
+                        tests.get(27).ExecuteTest(tests.get(27).getContent(), tests.get(27).getOutputContent());
                         break;
                     case (28):
-
+                        tests.get(28).ExecuteTest(tests.get(28).getContent(), tests.get(28).getOutputContent());
                         break;
                     case (29):
-
+                        tests.get(29).ExecuteTest(tests.get(29).getContent(), tests.get(29).getOutputContent());
                         break;
                     case (30):
-
+                        tests.get(30).ExecuteTest(tests.get(30).getContent(), tests.get(30).getOutputContent());
                         break;
                     case (31):
-
+                        tests.get(31).ExecuteTest(tests.get(31).getContent(), tests.get(31).getOutputContent());
                         break;
                     case (32):
-
+                        tests.get(32).ExecuteTest(tests.get(32).getContent(), tests.get(32).getOutputContent());
                         break;
                     default:
                         System.out.println("Wrong input given!");
