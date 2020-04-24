b668bcfc11cb458bd52f2ad376eff720ed1058c1
Ákos Schneider <koss644@gmail.com>
2020-04-24 13:21:25 +0200

    test

--- src/Prototype/Test.java
+++ src/Prototype/Test.java
@@ -1,4 +1,14 @@
 package Prototype;
 
-public class Test {
+public class Test{
+    private String name;
+    private int id;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
 }
--- src/Prototype/TestCases.java
+++ src/Prototype/TestCases.java
@@ -5,6 +5,16 @@ import java.util.*;
 public class TestCases {
     private List<Test> tests = new ArrayList<>();
 
+    public void loadTests(){
+
+    }
+
+    public void ListOutTests(){
+        for(Test t : tests){
+            System.out.println(t.getId() + ": " + t.getName());
+        }
+    }
+
     public void Test(){
 
     }
