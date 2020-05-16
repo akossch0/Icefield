573d1581862af5fe3e9ff6af70d8319379b0a72b
Balázs Blum <blumbalazs99@gmail.com>
2020-05-16 11:44:49 +0200

    Test Update

--- src/views/HoleView.java
+++ src/views/HoleView.java
@@ -3,14 +3,19 @@ package views;
 import Field.Hole;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 
 public class HoleView implements IView{
     Hole hole;
-    HoleView(Hole h){hole = h;}
+    BufferedImage image;
+    HoleView(Hole h){hole = h;
+        image = null;
+        System.out.println("Még nincs betöltve");
+    }
     @Override
     public void Draw(Graphics graphics) {
-
+        graphics.drawImage(image,hole.X*64,hole.Y*64,null);
     }
 
     @Override
--- src/views/IceBlockView.java
+++ src/views/IceBlockView.java
@@ -3,14 +3,21 @@ package views;
 import Field.IceBlock;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 
 public class IceBlockView implements IView {
     IceBlock iceBlock;
-    IceBlockView(IceBlock i){iceBlock = i;}
+
+    BufferedImage image;
+    IceBlockView(IceBlock i){
+        iceBlock = i;
+        image = null;
+        System.out.println("Még nincs betöltve");
+    }
     @Override
     public void Draw(Graphics graphics) {
-
+        graphics.drawImage(image,iceBlock.X*64,iceBlock.Y*64,null);
     }
 
     @Override
