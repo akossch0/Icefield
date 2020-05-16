6bfce6f4d616de2a1f859db9f9778a9ba62a3252
kp565 <petra.kalocsai12@gmail.com>
2020-05-16 12:03:25 +0200

    spadeview

--- src/views/SpadeView.java
+++ src/views/SpadeView.java
@@ -2,15 +2,20 @@ package views;
 
 import Item.Spade;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class SpadeView implements IView {
     Spade spade;
-    public SpadeView(Spade s){spade = s;}
+    BufferedImage image = ImageIO.read(new File("src/images/spade.png"));
+    public SpadeView(Spade s) throws IOException {spade = s;}
     @Override
     public void Draw(Graphics graphics) {
-
+        graphics.drawImage(image, spade.getField().X * 64, spade.getField().Y * 64, null);
     }
 
     @Override
