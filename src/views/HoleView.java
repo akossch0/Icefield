3979ea7a9afa12a51610c7ab63d87bb4bdc24aea
Bálint <takats>
2020-05-16 15:18:14 +0200

    src/images/...

--- src/views/HoleView.java
+++ src/views/HoleView.java
@@ -16,8 +16,8 @@ public class HoleView implements IView{
     public HoleView(Hole h){hole = h;
 
         try{
-            imagewithoutsnow  = (BufferedImage) ImageIO.read(new File("images/hole"));
-            imagewithsnow= (BufferedImage) ImageIO.read(new File("images/snow"));
+            imagewithoutsnow  = (BufferedImage) ImageIO.read(new File("src/images/hole"));
+            imagewithsnow= (BufferedImage) ImageIO.read(new File("src/images/snow"));
 
         }catch(IOException e){
             System.out.println("Baj van - HoleView betoltese");
