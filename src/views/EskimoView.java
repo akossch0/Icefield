342c683c2ddff04e296fb18d0d23dbb61176e942
Bálint <takats>
2020-05-17 19:43:07 +0200

    kék képek

--- src/views/EskimoView.java
+++ src/views/EskimoView.java
@@ -14,11 +14,13 @@ import java.io.IOException;
 public class EskimoView implements IView {
     Eskimo eskimo;
     BufferedImage image;
+    BufferedImage blueimage;
     EskimoView(Eskimo e){
 
         eskimo = e;
         try {
             image = (BufferedImage) ImageIO.read(new File("src/images/eskimo.png"));
+            blueimage = (BufferedImage) ImageIO.read(new File("src/images/eskimo_blue.png"));
 
         }
         catch(IOException ex) {
@@ -31,7 +33,10 @@ public class EskimoView implements IView {
         float index = field.getEntites().indexOf(eskimo);
         float entities = field.getEntites().size();
         int tilt = (int)(-10+index/entities*10 )>20? 20:(int)(-10+index/entities*10);
-        graphics.drawImage(image, (int) (field.X*64)+tilt, field.Y*64, null );
+        if (eskimo.isInWater())
+            graphics.drawImage(blueimage, (int) (field.X*64)+tilt, field.Y*64, null );
+        else
+            graphics.drawImage(image, (int) (field.X*64)+tilt, field.Y*64, null );
         if (GameplayFrame.getCurrentPlayer().equals(eskimo)){
             Graphics2D g2D = (Graphics2D) graphics;
             g2D.setColor(Color.red);
--- src/views/ResearcherView.java
+++ src/views/ResearcherView.java
@@ -13,9 +13,11 @@ import java.io.IOException;
 public class ResearcherView implements IView {
     Researcher researcher;
     BufferedImage image;
+    BufferedImage blueimage;
     ResearcherView(Researcher r){
         try {
             image = (BufferedImage) ImageIO.read(new File("src/images/researcher.png"));
+            blueimage = (BufferedImage) ImageIO.read(new File("src/images/researcher_blue.png"));
 
         }
         catch(IOException ex) {
@@ -29,7 +31,10 @@ public class ResearcherView implements IView {
         float index = field.getEntites().indexOf(researcher);
         float entities = field.getEntites().size();
         int tilt = (int)(-10+index/entities*10 )>20? 20:(int)(-10+index/entities*10);
-        graphics.drawImage(image, (int) (field.X*64)+tilt, field.Y*64, null );
+        if (researcher.isInWater())
+            graphics.drawImage(blueimage, (int) (field.X*64)+tilt, field.Y*64, null );
+        else
+            graphics.drawImage(image, (int) (field.X*64)+tilt, field.Y*64, null );
         if (GameplayFrame.getCurrentPlayer().equals(researcher)){
             Graphics2D g2D = (Graphics2D) graphics;
             g2D.setColor(Color.red);
