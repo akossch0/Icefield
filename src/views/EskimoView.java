b4cb82ee10c5159aa3b24831469a716a7530091c
Gergő Mészáros <meszaros.g999@gmail.com>
2020-05-17 19:03:18 +0200

    Color

--- src/views/EskimoView.java
+++ src/views/EskimoView.java
@@ -33,8 +33,10 @@ public class EskimoView implements IView {
         int tilt = (int)(-10+index/entities*10 )>20? 20:(int)(-10+index/entities*10);
         graphics.drawImage(image, (int) (field.X*64)+tilt, field.Y*64, null );
         if (GameplayFrame.getCurrentPlayer().equals(eskimo)){
-            graphics.setColor(Color.red);
-            graphics.drawRect(field.X*64,field.Y*64, 64,64);
+            Graphics2D g2D = (Graphics2D) graphics;
+            g2D.setColor(Color.red);
+            g2D.setStroke(new BasicStroke(4));
+            g2D.drawRect(field.X*64,field.Y*64, 64,64);
         }
         // graphics.drawImage(image, field.X*64, field.Y*64, null );
     }
